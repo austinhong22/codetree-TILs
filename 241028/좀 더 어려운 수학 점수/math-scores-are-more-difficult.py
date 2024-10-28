360a79f75ba124fb838b907cc1fdc98c636817af
austin <152476116+austinhong22@users.noreply.github.com>
2024-10-28 14:49:04 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241028/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+arr1 = input().split()
+arr2 = input().split()
+
+math_a = int(arr1[0])
+eng_a = int(arr1[1])
+math_b = int(arr2[0])
+eng_a = int(arr2[1])
+
+if math_a > math_b or (math_a == math_b and eng_a > eng_b):
+    print("A")
+else:
+    print("B")
