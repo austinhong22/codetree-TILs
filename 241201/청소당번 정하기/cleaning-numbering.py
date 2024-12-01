0bff38fb6a4d1cd3a241d640892fdfbdbf720a06
austin <152476116+austinhong22@users.noreply.github.com>
2024-12-01 19:21:33 +0900

    Title: 청소당번 정하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241201/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,14 @@
+cnt1 = 0
+cnt2 = 0
+cnt3 = 0
+n = int(input())
+
+for i in range(1,n+1):
+    if i % 12 == 0:
+        cnt3 +=1
+    elif i % 3 == 0 :
+        cnt2 +=1
+    elif i % 2 == 0:
+        cnt1 +=1
+
+print(f"{cnt1} {cnt2} {cnt3}")
