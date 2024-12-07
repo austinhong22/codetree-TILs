0b6116f9c7926c6c8035be2af3c9c776cc9f69ae
austin <152476116+austinhong22@users.noreply.github.com>
2024-12-07 20:18:00 +0900

    Title: 모두 3의 배수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241207/모두 3의 배수/all-multiples-of-3.py
+++ 241207/모두 3의 배수/all-multiples-of-3.py
@@ -1,10 +1,8 @@
-satisfied = False
+satisfied = True
 
 for i in range(5):
     a = int(input())
-    if a % 3 == 0:
-        satisfied = True
-    else:
+    if a % 3 !=0:
         satisfied = False
 
 if satisfied == True:
