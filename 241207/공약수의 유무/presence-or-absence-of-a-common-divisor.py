c35a542d63a21b6a23d060fb12ea9d2eb143305d
austin <152476116+austinhong22@users.noreply.github.com>
2024-12-07 20:06:45 +0900

    Title: 공약수의 유무, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241207/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 241207/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,17 +1,9 @@
-from math import gcd
-
-# 입력 받기
 a, b = map(int, input().split())
 
-# 1920과 2880의 최대공약수 계산
-common_gcd = gcd(1920, 2880)
-
-# 공약수 검사
 found = 0
 for i in range(a, b+1):
-    if i % common_gcd == 0:
+    if 1920 % i == 0 and 2880 % i == 0:  # i가 두 수 모두를 나누어떨어뜨리는지 검사
         found = 1
         break
 
-# 결과 출력
 print(found)
