7d664e1af040f323f8aae9aa239d57971cf379f5
austin <152476116+austinhong22@users.noreply.github.com>
2024-12-07 20:12:28 +0900

    Title: 소수 판별기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241207/소수 판별기/prime-number-discriminator.py
+++ 241207/소수 판별기/prime-number-discriminator.py
@@ -1,13 +1,14 @@
 n = int(input())
 satisfied = False
 
-for i in range(1,n):
+for i in range(2,n):
     if n % i == 0:
         satisfied = False
+        break
     else:
         satisfied = True
 
 if satisfied == True:
     print("P")
-else:
+elif satisfied == False:
     print("C")
