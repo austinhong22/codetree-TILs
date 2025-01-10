d735ffb04950f57f99a26ae18feb6453484938c8
austin <152476116+austinhong22@users.noreply.github.com>
2025-01-10 23:48:02 +0900

    Title: 학점 계산기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250110/학점 계산기/credit-calculator.py
@@ -0,0 +1,21 @@
+
+n = int(input())
+
+
+arr = list(map(float, input().split()))
+	
+
+sum_val = sum(arr)
+
+
+avg = sum_val / n
+
+
+print(f"{avg:.1f}")
+
+if avg >= 4.0:
+	print("Perfect")
+elif avg >= 3.0:
+	print("Good")
+else:
+	print("Poor")
